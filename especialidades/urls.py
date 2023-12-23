570d85ae97eeb6f4b7367be2a4eecf739ad0c230
ThiagoTerraDev <thiagoterradev@gmail.com>
2023-12-23 11:32:47 -0300

    Implementacao do JavaScript para performar relacao no front-end entre especialidade e profissional

--- especialidades/urls.py
+++ especialidades/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('solicitar_consultas/', views.solicitar_consultas, name='solicitar_consultas')
+    path('solicitar_consultas/', views.solicitar_consultas, name='solicitar_consultas'),
+    path('solicitar_profissionais/', views.solicitar_profissionais, name='solicitar_profissionais')
 ]
--- especialidades/views.py
+++ especialidades/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.contrib.auth.decorators import login_required
-from .models import Especialidade
+from .models import Especialidade, Profissional
+from django.core.serializers import serialize
 # from .models import StatusConsulta
 
 
@@ -10,5 +11,21 @@ def solicitar_consultas(request):
     if request.method == "GET":
         especialidades = Especialidade.objects.all()
         return render(request, 'solicitar_consultas.html', {'especialidades': especialidades})
+
+
+def solicitar_profissionais(request):
+    if request.method == "GET":
+        profissionais = Profissional.objects.all()
+        dados_profissionais = []
+
+        for profissional in profissionais:
+            dados_profissional = {
+                'nome': profissional.nome,
+                'especialidades': profissional.especialidades.id
+            }
+            dados_profissionais.append(dados_profissional)
+        
+        return JsonResponse(dados_profissionais, safe=False)
+
 
 # Create your views here.
